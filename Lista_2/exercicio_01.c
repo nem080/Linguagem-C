59b2b4a54aa6e73781379a3205affb9be88f7c1f
Rogisvaldo Js <73315533+nem080@users.noreply.github.com>
2020-12-25 21:57:36 -0300

    questionario_exerc

--- Lista_2/exercicio_01.c
+++ Lista_2/exercicio_01.c
@@ -1,5 +1,7 @@
  /*Exercicio_01*/
- 
+ /*Faça um programa que imprima um menu de 4 pratos na tela e uma quinta opção para sair do pro- grama. O programa deve imprimir o prato 
+ solicitado. O programa deve terminar quando for escolhido a quinta opção.*/
+
  #include <stdio.h>
  #include <stdlib.h>
 
